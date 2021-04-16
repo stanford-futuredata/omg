a66ae084c3d0f277c0831c9c58086acefa6b597c
Daniel Kang <daniel.d.kang@gmail.com>
2021-04-15 23:47:03 -0700

    fix some bugs, coloring

--- omg/consistency.py
+++ omg/consistency.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from collections import defaultdict
 
 
@@ -11,10 +12,12 @@ class IdentifierConsistencyAssertion(object):
         return self.name
 
     def check_errors(self, inps, outs):
+        joined = pd.concat([inps.reset_index(drop=True), outs.reset_index(drop=True)], axis=1)
+        joined = joined.loc[:, ~joined.columns.duplicated()]
         errors = []
         seen_pairs = defaultdict(lambda: defaultdict(list))
-        for idx, ((_, inp), (_, out)) in enumerate(zip(inps.iterrows(), outs.iterrows())):
-            seen_pairs[self.identifier][out[self.attribute]].append(idx)
+        for idx, row in joined.iterrows():
+            seen_pairs[row[self.identifier]][row[self.attribute]].append(idx)
 
         for iden_val, attr_to_idx in seen_pairs.items():
             if len(attr_to_idx) > 1:
@@ -35,7 +38,7 @@ class TimeConsistencyAssertion(object):
         self.timestamp = timestamp
         self.window = window
         self.allowed_transitions = allowed_transitions
-        self.name = f'Time consistency `{identifier}`, window of {window}'
+        self.name = f'Time consistency `{identifier}` over `{timestamp}`, window of {window}'
 
     def get_name(self):
         return self.name
@@ -46,18 +49,18 @@ class TimeConsistencyAssertion(object):
         for iden_val, group in groups:
             timestamp_vals = group[self.timestamp].values
             timestamp_vals.sort()
-            timestamp_vals.sort()
             for i, ts_start in enumerate(timestamp_vals):
                 nb_transitions = 0
                 for j, ts_other in enumerate(timestamp_vals[i + 1:]):
                     j += i + 1
-                    if ts_other - ts_start > self.window:
+                    if ts_other - ts_start >= self.window:
                         break
                     if timestamp_vals[j] != timestamp_vals[j - 1] + 1:
                         nb_transitions += 2
-                if nb_transitions > self.allowed_transitions:
-                    err = group[self.timestamp].index[0]
-                    errors.append(err)
+                    if nb_transitions > self.allowed_transitions:
+                        err = [group[self.timestamp].index[i], group[self.timestamp].index[j]]
+                        errors.append(err)
+                        break
         return errors
 
     def get_assertion(self):
